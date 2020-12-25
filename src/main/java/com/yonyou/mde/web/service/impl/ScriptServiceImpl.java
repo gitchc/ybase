28c24c0f4702350986a2555abbc7f1070e344f14
chenghch <chenghch@yonyou.com>
2020-12-25 14:57:10 +0800

    屏蔽造数文件

--- src/main/java/com/yonyou/mde/web/service/impl/ScriptServiceImpl.java
+++ src/main/java/com/yonyou/mde/web/service/impl/ScriptServiceImpl.java
@@ -1,15 +1,16 @@
 package com.yonyou.mde.web.service.impl;
 
-import com.yonyou.mde.web.script.Utils.KeyWord;
 import com.yonyou.mde.web.core.AbstractService;
 import com.yonyou.mde.web.core.ScriptException;
 import com.yonyou.mde.web.core.ServiceException;
+import com.yonyou.mde.web.dao.ScriptMapper;
 import com.yonyou.mde.web.model.Completer;
 import com.yonyou.mde.web.model.Script;
 import com.yonyou.mde.web.model.ScriptVo;
+import com.yonyou.mde.web.script.Utils.KeyWord;
+import com.yonyou.mde.web.script.classloder.JavaClassUtils;
 import com.yonyou.mde.web.service.ScriptService;
 import com.yonyou.mde.web.utils.SnowID;
-import com.yonyou.mde.web.script.classloder.JavaClassUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -27,11 +28,11 @@ import java.util.Map;
 @Transactional
 public class ScriptServiceImpl extends AbstractService<Script> implements ScriptService {
     @Resource
-    private com.yonyou.mde.web.dao.ScriptMapper ScriptMapper;
+    private ScriptMapper scriptMapper;
 
     @Override
     public void insertScript(Script script) {
-        Script oldsc = ScriptMapper.getScriptByName(script.getName());
+        Script oldsc = scriptMapper.getScriptByName(script.getName());
         if (oldsc != null) {
             throw new ServiceException("脚本名称不能重复!");
         } else {
@@ -44,11 +45,11 @@ public class ScriptServiceImpl extends AbstractService<Script> implements Script
 
     @Override
     public void updateName(Script script) {
-        Script oldsc = ScriptMapper.getScriptByName(script.getName());
+        Script oldsc = scriptMapper.getScriptByName(script.getName());
         if (oldsc != null) {
             throw new ServiceException("脚本名称不能重复!");
         } else {
-            ScriptMapper.updateName(script.getId(), script.getName());
+            scriptMapper.updateName(script.getId(), script.getName());
         }
 
     }
@@ -56,7 +57,7 @@ public class ScriptServiceImpl extends AbstractService<Script> implements Script
     @Override
     public void updateContent(Script script) {
         String nowuser = "admin";
-        ScriptMapper.updateContent(script.getId(), script.getContent(), nowuser);
+        scriptMapper.updateContent(script.getId(), script.getContent(), nowuser);
         checkContent(script);
     }
 
@@ -66,18 +67,18 @@ public class ScriptServiceImpl extends AbstractService<Script> implements Script
         String id = vo.getId();
         String name = vo.getName();
         if (StringUtils.isNotBlank(id)) {
-            script = ScriptMapper.selectByPrimaryKey(id);
+            script = scriptMapper.selectByPrimaryKey(id);
         } else if (StringUtils.isNotBlank(name)) {
-            script = ScriptMapper.getScriptByName(name);
+            script = scriptMapper.getScriptByName(name);
             id = script.getId();
         } else {
             throw new ScriptException("名称或者id不能为空");
         }
         try {
-            ScriptMapper.updateStatus(id, 1);
+            scriptMapper.updateStatus(id, 1);
             return run(script, vo.getVars());
         } catch (Exception e) {
-            ScriptMapper.updateStatus(id, 2);
+            scriptMapper.updateStatus(id, 2);
             Map<String, Object> res = new HashMap<>();
             res.put("code", "-1");
             res.put("msg", e.getMessage());
