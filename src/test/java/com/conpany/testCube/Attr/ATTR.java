0a8433454508b26a5e97939d0183b021fd767e6c
chenghch <chenghch@yonyou.com>
2021-01-07 19:47:36 +0800

    table布局

--- src/test/java/com/conpany/testCube/Attr/ATTR.java
+++ src/test/java/com/conpany/testCube/Attr/ATTR.java
@@ -23,26 +23,35 @@ public class ATTR extends Tester {
     public void exp() throws MdeException {
         Cube cube = Server.getServer().getCube("测试属性");
         Dimension dimension = cube.getDimension("QIJIAN");
+        testAnd(cube, dimension);//测试if and
+        testOr(cube, dimension);//测试if or
+        testContinue(cube, dimension); //测试if continue
+
+//        qiantao(cube, dimension);//嵌套,if 暂时不支持
+
+
+    }
+
+    private void qiantao(Cube cube, Dimension dimension) throws MdeException {
         SliceResult sliceResult;
-        cube.exp("VERSION.第1版=if(attrValue(QIJIAN,~,所属季度)==1,1,100)");
+        cube.exp("VERSION.第1版=if(attrValue(QIJIAN,~,所属季度)==1,if(attrValue(QIJIAN,~,所属月)==1,1,100),100)");
         sliceResult = cube.find("VERSION.第1版");
+        printAll(sliceResult);
         for (Row row : sliceResult.toTable()) {
             Member member = dimension.getMember(row.getText("qijian"));
             String value = member.getAttrValue("所属季度");
-            if (value.equals("1")) {
+            String value1 = member.getAttrValue("所属月");
+            if (value.equals("1") && value1.equals(1)) {
                 Assert.assrt("属性赋值不正确!", row.getDouble("value") == 1);
             } else {
                 Assert.assrt("属性赋值不正确!", row.getDouble("value") == 100);
             }
         }
-        cube.exp("VERSION.第1版=attrValue(QIJIAN,~,所属月)");
-        sliceResult = cube.find("VERSION.第1版");
-        for (Row row : sliceResult.toTable()) {
-            Member member = dimension.getMember(row.getText("qijian"));
-            String value = member.getAttrValue("所属月");
-            Double dvalue = Double.parseDouble(value);
-            Assert.assrt("属性赋值不正确!", dvalue == row.getDouble("value"));
-        }
+    }
+
+    private void testContinue(Cube cube, Dimension dimension) throws MdeException {
+        SliceResult sliceResult;
+        cube.exp("VERSION.第1版=if(true,2,1)");//初始化数据
         cube.exp("VERSION.第1版=if(attrValue(QIJIAN,~,所属月)==2,continue,100)");
         sliceResult = cube.find("VERSION.第1版");
         for (Row row : sliceResult.toTable()) {
@@ -52,6 +61,40 @@ public class ATTR extends Tester {
                 Assert.assrt("属性赋值不正确!", row.getDouble("value") == 100);
             } else {
                 Assert.assrt("属性赋值不正确!", row.getDouble("value") == 2);
+            }
+        }
+    }
+
+    private void testOr(Cube cube, Dimension dimension) throws MdeException {
+        SliceResult sliceResult;
+        cube.exp("VERSION.第1版=if((attrValue(QIJIAN,~,所属季度)==1 || attrValue(QIJIAN,~,所属月)==1),1,100)");
+        sliceResult = cube.find("VERSION.第1版");
+        printAll(sliceResult);
+        for (Row row : sliceResult.toTable()) {
+            Member member = dimension.getMember(row.getText("qijian"));
+            String value = member.getAttrValue("所属季度");
+            String value1 = member.getAttrValue("所属月");
+            if (value.equals("1") || value1.equals("1")) {
+                Assert.assrt("属性赋值不正确!", row.getDouble("value") == 1);
+            } else {
+                Assert.assrt("属性赋值不正确!", row.getDouble("value") == 100);
+            }
+        }
+    }
+
+    private void testAnd(Cube cube, Dimension dimension) throws MdeException {
+        SliceResult sliceResult;
+        cube.exp("VERSION.第1版=if((attrValue(QIJIAN,~,所属季度)==1 && attrValue(QIJIAN,~,所属月)==1),1,100)");
+        sliceResult = cube.find("VERSION.第1版");
+        printAll(sliceResult);
+        for (Row row : sliceResult.toTable()) {
+            Member member = dimension.getMember(row.getText("qijian"));
+            String value = member.getAttrValue("所属季度");
+            String value1 = member.getAttrValue("所属月");
+            if (value.equals("1") && value1.equals("1")) {
+                Assert.assrt("属性赋值不正确!", row.getDouble("value") == 1);
+            } else {
+                Assert.assrt("属性赋值不正确!", row.getDouble("value") == 100);
             }
         }
     }
