914682ee338ebd8f10975ad4878b5f6f1daaf54f
chenghch <chenghch@yonyou.com>
2021-01-19 20:36:53 +0800

    集席数据修复bug

--- src/main/java/com/yonyou/mde/web/service/DataService/CubeDataService.java
+++ src/main/java/com/yonyou/mde/web/service/DataService/CubeDataService.java
@@ -56,7 +56,7 @@ public class CubeDataService {
         List<Member[]> rowslice = new MuiltCross(rowDims).get(0, 1000);//获取维度前1000条笛卡尔积
         List<Member[]> colslice = new MuiltCross(colDims).getAll();//获取列维度的笛卡尔积
 
-        String queryExp = getQueryExp(cubeid, pageFindStr, rowslice, dimMap);//根据前1000条获取要查询的切片组合
+        String queryExp = getQueryExp(pageFindStr, rowslice, colDims, dimMap);//根据前1000条获取要查询的切片组合
         Map<String, Object> datas = getDatas(cubeCode, rowArr, colArr, queryExp, dimMap);//根据切片和行列,取值
         List<String> colFields = getFileds(colslice, dimMap);//获取拼接表格的filed
         List<Map<String, Object>> results = getTableData(pageFindStr, dimMap, rowslice, datas, colFields);//获取Table的键对值
@@ -81,17 +81,19 @@ public class CubeDataService {
             for (Member member : rowMembers) {//拼接维度成员
                 String dimcode = dimMap.get(member.getDimid());
                 String field = StrUtil.format("{}.{}", dimcode, member.getCode());
-                sb.append("#");
+                if (sb.length() > 0) {
+                    sb.append("#");
+                }
                 sb.append(field);
                 rs.put(dimcode, MemberUtil.getLevelName(member));
             }
             if (StringUtils.isNotBlank(pageFindStr)) {
-                rs.put("rowkey", pageFindStr + sb.toString());
+                rs.put("rowkey", pageFindStr.concat("#").concat(sb.toString()));
             } else {
-                rs.put("rowkey", sb.toString());
+                    rs.put("rowkey", sb.toString());
             }
             for (String colfield : colFields) {//拼接数据列
-                StringBuilder newSb = new StringBuilder();
+                StringBuilder newSb = new StringBuilder("#");
                 newSb.append(sb);
                 newSb.append("#");
                 newSb.append(colfield);
@@ -174,20 +176,51 @@ public class CubeDataService {
      * @return: void
      * @author chenghch
      */
-    private String getQueryExp(String cubeid, String pageFindStr, List<Member[]> rowslice, Map<String, String> dimMap) {
+    private String getQueryExp(String pageFindStr, List<Member[]> rowslice, List<Member[]> colslice, Map<String, String> dimMap) {
         Map<String, Set<String>> findRowMembers = getFindRowMembers(rowslice);//根据笛卡尔积组合,获取到切片集合
         StringBuilder findStr = new StringBuilder();
         findStr.append(pageFindStr);
+        StringBuilder colexp = getColExp(colslice, dimMap);
+        StringBuilder rowexp = getRowExp(findRowMembers, dimMap);
+        findStr.append(colexp);
+        findStr.append(rowexp);
+        if (StringUtils.isBlank(pageFindStr)) {
+            findStr.deleteCharAt(0);
+        }
+        return findStr.toString();
+    }
+
+    private StringBuilder getColExp(List<Member[]> colslice, Map<String, String> dimMap) {
+        StringBuilder colexps = new StringBuilder();
+        for (Member[] members : colslice) {
+            StringBuilder colexp = new StringBuilder();
+            for (Member member : members) {
+                if (colexp.length() == 0) {
+                    colexp.append("#");
+                    colexp.append(dimMap.get(member.getDimid()));
+                    colexp.append(".[");
+                    colexp.append(member.getCode());
+                } else {
+                    colexp.append(",");
+                    colexp.append(member.getCode());
+                }
+            }
+            colexp.append("]");
+            colexps.append(colexp);
+        }
+        return colexps;
+    }
+
+    private StringBuilder getRowExp(Map<String, Set<String>> findRowMembers, Map<String, String> dimMap) {
+        StringBuilder findStr = new StringBuilder();
         for (Map.Entry<String, Set<String>> entry : findRowMembers.entrySet()) {
             String dimid = entry.getKey();
             Set<String> members = entry.getValue();
             String rowFindStr = StrUtil.format("{}.[{}]", dimMap.get(dimid), StringUtils.join(members, ","));
-            if (findStr.length() > 0) {
-                findStr.append("#");
-            }
+            findStr.append("#");
             findStr.append(rowFindStr);
         }
-        return findStr.toString();
+        return findStr;
     }
 
     /**
