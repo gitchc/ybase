ad448312aabdf3e31633ee87f88efd55ad8828bc
chenghch <chenghch@yonyou.com>
2021-01-27 20:08:45 +0800

    视图保存完成

---
+++ src/main/java/com/yonyou/mde/web/model/types/LayoutType.java
@@ -0,0 +1,13 @@
+package com.yonyou.mde.web.model.types;
+
+/**
+ * @version 1.0
+ * @Description: 类描述
+ * @Author chenghch
+ * @Date 2021/1/27 19:22
+ */
+public class LayoutType {
+    public static final String PAGE="page";//页面维
+    public static final String COL="col";//列维
+    public static final String ROW="row";//行维
+}
